b28b9cc7e4f1de060c801d7b38b1e2d1966ceaaa
miguel <miguelrusso2840@gmail.com>
2023-08-30 12:59:24 -0300

    subo nueva actualizacion

--- script.js
+++ script.js
@@ -35,15 +35,38 @@ function cargarEventListeners() {
  function insertarCarrito(elemento) {
 
     const row = document.createElement('tr');
-    row.innerHTML = 
-
+    row.innerHTML = `
     <td>
-        < img src = '${elemento.imagen}'  width100 />
+        < img src = '${elemento.imagen}'  width=100 />
     </td>
-     
-
-
-
-    
-
+    <td>
+        ${elemento.titulo}    
+    </td> 
+    <td>
+        ${elemento.precio}    
+    </td>
+    <td>
+        <a href ="#" class ="borrar" data-id="${elemento.id}">x </a>
+    </td>
+    ` ;
+    lista.appendChild(row);
  }
+
+ function eliminarElemento(e) {
+    e.preventDefault();
+    let elemento,
+        elementoId;
+    if(e.target.classList.contains('borrar')) {
+        e.target.parentElement.parentElement.remove();
+            elemento =e.target.parentElement.parentElement;
+            elementoId = elemento.querySelector('a').getAttribute('data-id');
+                                                                            
+        }
+    }
+  
+    function vaciarCarrito() {
+        while(lista.firstChild) {
+            lista.removeChild(lista.firstChild);
+        }
+        return false;
+    }
