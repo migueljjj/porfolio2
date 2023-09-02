597ffa07d3bb84636dc8e87608bdc537b97bbdfa
miguel <miguelrusso2840@gmail.com>
2023-09-01 22:11:04 -0300

    subo nueva actualizacion

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function cargarEventListeners() {
 
  function leerDatosElemento(elemento) {
     const infoElemento = {
-        imgen: elemento.querySelector('img').src,
+        imagen: elemento.querySelector('img').src,
         titulo: elemento.querySelector('h3').textContent,
         precio: elemento.querySelector('.precio').textContent,
         id: elemento.querySelector('a').getAttribute('data-id')
